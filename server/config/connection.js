a859312b6e958c8c2e231feeaa7f68f024f4761c
brian nelson <bn3l10@gmail.com>
2022-12-28 15:09:09 -0500

    deploy

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/tmtshop', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/tmtshop', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
